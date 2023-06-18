9574e12c462107daa2f629962a121a3a4a18261b
Ayushi Rhutwik Pohekar <ayushi2021.29@gmail.com>
2023-06-18 22:30:28 +0100

    data saved in mongodb

--- config/db.js
+++ config/db.js
@@ -1,22 +1,63 @@
+
+
+
 import mongoose from "mongoose";
 import colors from "colors";
 import { data } from "../data.js/MovieData.js";
+import movieModel from "../models/movieModel.js";
+import producerModel from "../models/producerModel.js";
+import actorModel from "../models/actorModel.js";
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(
-      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+      `Connected to MongoDB Database ${conn.connection.host}`.bgMagenta.white
     );
-  console.log(data)
 
+    // Iterate over the data array
+    for (const jsonObject of data) {
+      // Check if the document with the same unique identifier already exists
+      const existingMovie = await movieModel.findOne({
+        uniqueIdentifier: jsonObject.movie.uniqueIdentifier,
+      });
 
+      // If the document already exists, skip saving
+      if (existingMovie) {
+        console.log(
+          `Data already exists for: ${jsonObject.movie.original_title}`
+        );
+        continue;
+      }
 
+      // Create and save the new documents
+      const producer = new producerModel(jsonObject.producer);
+      await producer.save();
 
+      const movie = new movieModel(jsonObject.movie);
+      movie.producer = producer._id;
 
+      const actorReferences = [];
 
+      for (const actorData of jsonObject.actors) {
+        const actor = new actorModel(actorData);
+        actor.movies.push(movie._id);
+        await actor.save();
+        actorReferences.push(actor._id);
+      }
+
+      movie.actors = actorReferences;
+      await movie.save();
+
+      producer.movies.push(movie._id);
+      await producer.save();
+
+      console.log('Data saved for:', jsonObject.movie.original_title);
+    }
+
+    console.log('All data saved successfully');
   } catch (error) {
-    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+    console.log(`Error in MongoDB: ${error}`.bgRed.white);
   }
 };
 
